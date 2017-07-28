fed50ea418827dccaf4b0a07493af1bae421533a
Angel <Angel@DESKTOP-9QUHPFF>
2017-07-28 22:45:29 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1, 3));
+        }
+        static int Add(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
